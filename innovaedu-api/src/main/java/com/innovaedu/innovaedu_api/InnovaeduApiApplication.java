487411acb49947f5982bbefe30aaeb85ca441bf7
JordySamirC <jordy.cervera.dev@gmail.com>
2025-11-22 17:55:24 -0500

    first commit

---
+++ innovaedu-api/src/main/java/com/innovaedu/innovaedu_api/InnovaeduApiApplication.java
@@ -0,0 +1,13 @@
+package com.innovaedu.innovaedu_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaeduApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnovaeduApiApplication.class, args);
+	}
+
+}
---
+++ innovaedu-api/src/test/java/com/innovaedu/innovaedu_api/InnovaeduApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovaedu.innovaedu_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovaeduApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
